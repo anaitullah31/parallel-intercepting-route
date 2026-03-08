25cc886466b90c8307f2db15c0c9f9e24d40cd23
anaitullah190 <anaitullah190@gmail.com>
2026-03-08 08:15:45 +0800

    Implement parallel and intercepting route

---
+++ app/gallery/images.js
@@ -0,0 +1,47 @@
+import photo1 from "./photos/1.jpg";
+import photo2 from "./photos/2.jpg";
+import photo3 from "./photos/3.jpg";
+import photo4 from "./photos/4.jpg";
+import photo5 from "./photos/5.jpg";
+import photo6 from "./photos/6.jpg";
+import photo7 from "./photos/7.jpg";
+
+const images = [
+  {
+    id: "1",
+    name: "Nature - 1",
+    src: photo1,
+  },
+  {
+    id: "2",
+    name: "Nature - 2",
+    src: photo2,
+  },
+  {
+    id: "3",
+    name: "Nature - 3",
+    src: photo3,
+  },
+  {
+    id: "4",
+    name: "Nature - 4",
+    src: photo4,
+  },
+  {
+    id: "5",
+    name: "Nature - 5",
+    src: photo5,
+  },
+  {
+    id: "6",
+    name: "Nature - 6",
+    src: photo6,
+  },
+  {
+    id: "7",
+    name: "Nature - 7",
+    src: photo7,
+  },
+];
+
+export default images;
